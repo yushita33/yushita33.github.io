e6c9d8dac026aa69955c9c0e87c0a0e47d10f674
yushita <sadaharunn@gmail.com>
2025-06-29 12:23:31 +0900

    ヘッダーの固定および共通CSSをcommon.cssに移す

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -3,14 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlay = document.getElementById('menuOverlay');
     const links = overlay.querySelectorAll('a');
 
+    const switchScroll = () => {
+        if (overlay.classList.contains('active')) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }
+
     hamburger.addEventListener('click', () => {
-      overlay.classList.toggle('active');
+        overlay.classList.toggle('active');
+        // スクロール制御を追加
+        document.body.classList.toggle("no-scroll");
+        switchScroll();
     });
 
     // メニュー内リンククリック or 背景クリックで閉じる
     overlay.addEventListener('click', (e) => {
         if (e.target === overlay || e.target.tagName === 'A') {
             overlay.classList.remove('active');
+            switchScroll();
         }
     });
 });
